afac779d198b15bc5458c052d5725477b071f9ff
monmima <monmima@protonmail.com>
2020-06-06 13:20:18 -0400

    now at 17:21 in Web Dev Simplified's tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from "./TodoList"
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
-  return (
-    <TodoList />
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
 
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name == "") return
+    console.log(name);
+  }
+
+  return (
+    <div>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </div>
   ) 
 }
 
